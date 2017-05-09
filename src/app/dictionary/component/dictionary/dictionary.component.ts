eb6c691d15736316e7e10083991b91467ccf85c3
YuryYaroshevich <yury.yaroshevich@gmail.com>
2017-05-09 12:42:02 +0300

    ability to edit dictionary on view page

--- src/app/dictionary/component/dictionary/dictionary.component.ts
+++ src/app/dictionary/component/dictionary/dictionary.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
 import { Dictionary } from '../../model/dictionary';
+import { DictionaryEntry } from '../../model/dictionary-entry';
 import { DictionaryService } from '../../service/dictionary.service';
+import { DictionaryForm } from '../new-dictionary/dictionary-form';
 
 @Component({
     selector: 'dictionary',
     templateUrl: 'dictionary.component.html',
     styleUrls: ['dictionary.component.css']
 })
-export class DictionaryComponent implements OnInit, OnDestroy {
-    subscription: Subscription;
-
-    dictionary: Dictionary;
-
+export class DictionaryComponent extends DictionaryForm implements OnInit, OnDestroy {
     constructor(private dictionaryService: DictionaryService,
-                private activatedRoute: ActivatedRoute) {}
+                private activatedRoute: ActivatedRoute,
+                private router: Router) {
+        super();
+    }
 
     ngOnInit(): void {
         this.subscription = this.activatedRoute.params.subscribe(
             (params:any) => {
                 this.dictionaryService.getDictionary(params['id'])
                     .subscribe(
-                        dictionary => {
-                            this.dictionary = dictionary
-                        },
+                        dictionary => this.dictionary = dictionary,
                         error => console.log(error)
                     );
             });            
+    }
+
+    save() {
+        this.dictionaryService.updateDictionary(this.dictionary)
+            .subscribe(
+                dictionary => this.ngOnInit(),//this.router.navigate(['dictionary', dictionary.id]),
+                error => console.log(error)
+            );
     }
 
     ngOnDestroy() {
